9ba1fc02cbabc47bd3a9fef2e15a57a1506a8398
will23JD <will_jd@outlook.com>
2022-08-20 22:32:38 +0000

    Added blog detail view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -11,3 +11,15 @@ def blog(request):
     }
 
     return render(request, 'blog/blog.html', context)
+
+
+def blog_detail(request, blog_id):
+    """ Return individual blog details """
+
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+
+    context = {
+        'blog_detail': blog_detail,
+    }
+
+    return render(request, 'blog/blog_detail.html', context)
