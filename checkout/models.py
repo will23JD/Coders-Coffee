9a791158307f274740073a6599f0e57771f5e3fc
will23JD <will_jd@outlook.com>
2022-08-21 11:47:16 +0000

    Bug fix size, max_length error

--- checkout/models.py
+++ checkout/models.py
@@ -65,7 +65,7 @@ class Order(models.Model):
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
+    product_size = models.CharField(max_length=5, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
@@ -79,7 +79,7 @@ class OrderLineItem(models.Model):
             self.lineitem_total = finalprice * self.quantity
         else:
             self.lineitem_total = self.product.price * self.quantity
-        super().save(*args, **kwargs)
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
