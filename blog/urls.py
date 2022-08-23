cde0d4fb3e1d5f72cb43d19ad0ed50f94623b187
will23JD <will_jd@outlook.com>
2022-08-23 12:27:23 +0000

    Added edit blog view, template and form

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.blog, name='blog'),
     path('<int:blog_id>/', views.blog_detail, name='blog_detail'),
     path('add/', views.add_blog, name='add_blog'),
+    path('edit/<int:blog_id>/', views.edit_blog, name='edit_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -54,3 +54,33 @@ def add_blog(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_blog(request, blog_id):
+    """ Edit a blog in the store """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only an admin can access this page.')
+        return redirect(reverse('home'))
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated Blog!')
+            return redirect(reverse('blog_detail', args=[blog.id]))
+        else:
+            messages.error(request, 'Failed to update Blog. Please ensure the form is valid.')
+    else:
+        form = BlogForm(instance=blog)
+        messages.info(request, f'You are editing {blog.title}')
+
+    template = 'blog/edit_blog.html'
+    context = {
+        'form': form,
+        'blog': blog,
+    }
+
+    return render(request, template, context)
