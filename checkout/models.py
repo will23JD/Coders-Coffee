d42b2cb3f0a65873df44bc4348af6b34fcb56947
will23JD <will_jd@outlook.com>
2022-08-23 19:27:08 +0000

    Fix styles on blogs and heroku fix

--- checkout/models.py
+++ checkout/models.py
@@ -67,7 +67,7 @@ class OrderLineItem(models.Model):
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=5, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=6, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
