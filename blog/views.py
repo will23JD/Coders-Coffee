2871312fa62f192363d6ceb86a8e1fcd87661192
will23JD <will_jd@outlook.com>
2022-08-25 12:15:32 +0000

    Added liked and unliked view and template to blog

--- blog/views.py
+++ blog/views.py
@@ -22,10 +22,14 @@ def blog_detail(request, blog_id):
 
     blog_detail = get_object_or_404(Blog, pk=blog_id)
     comments = blog_detail.comments.order_by("-created_on")
+    liked = False
+    if blog_detail.likes.filter(id=request.user.id).exists():
+        liked = True
 
     context = {
         'blog_detail': blog_detail,
         'comments': comments,
+        "liked": liked,
         'commentform': CommentForm(),
     }
 
